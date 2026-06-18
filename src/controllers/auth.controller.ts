07d0b4df15c3ef59e25d2dc7671b2c221aac1870
Jayan Shrestha <jayanshr44@gmail.com>
2026-06-19 09:00:28 +1000

    Add validation for user registration and login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,6 +4,9 @@ import {registerUser as register, loginUser as login, checkUserExists} from "../
 export async function registerUser(req: Request, res: Response){
     try{
         const {email, username, password} = req.body;
+          if (!email || !username || !password) {
+            return res.status(400).json({ error: "email, username and password are required" });
+       }
         const userExists = await checkUserExists(email);
         if (userExists) {
             return res.status(400).json({ error: "User already exists" });
@@ -23,7 +26,10 @@ export async function registerUser(req: Request, res: Response){
 export async function loginUser(req: Request, res: Response){
     try{
         const {email, password} = req.body;
-          const userExists = await checkUserExists(email);
+          if (!email || !username || !password) {
+         return res.status(400).json({ error: "email and password are required" });
+       }
+        const userExists = await checkUserExists(email);
         if (!userExists) {
             return res.status(400).json({ error: "User does not exist" });
         }
@@ -34,6 +40,6 @@ export async function loginUser(req: Request, res: Response){
     }
     catch(error){
         console.error(`Error logging in user: ${error}`);
-        res.status(500).json({ error: `Incorrect password, Failed to log in User` });
+        res.status(401).json({ error: "Incorrect Password, Failed to login" });
     }       
 }
