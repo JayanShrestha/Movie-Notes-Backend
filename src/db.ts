cb6208b10eac18bfa9f72247638f2d0f71950ea4
Jayan Shrestha <jayanshr44@gmail.com>
2026-08-01 23:01:52 +1000

    updated neon+prisma to use websockets for DB connection on cloud run

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,18 @@
 import 'dotenv/config'
 import { PrismaClient } from './generated/prisma/client.js'
 import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
-const connectionString = process.env.DATABASE_URL
+//Required for Docker + CLoud Run
+neonConfig.webSocketConstructor = ws;
+//Use pooled Neon connection strings
+const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   throw new Error("Missing required env var: DATABASE_URL")
 }
 
-const adapter = new PrismaNeon({
-  connectionString,
-})
+const prisma = new PrismaClient({ adapter: new PrismaNeon({connectionString}),
+});
 
-export const prisma = new PrismaClient({ adapter })
+export { prisma };
